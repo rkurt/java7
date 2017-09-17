4fea6d0b5fbad2f836f1e97d04482c792b83153f
rkurt <rafalkurt@gmail.com>
2017-09-17 19:51:57 +0200

    Ćwiczenie 1

---
+++ employment/src/employees/Employee.java
@@ -0,0 +1,10 @@
+package employees;
+
+import java.util.Date;
+
+public class Employee {
+    public static void main(String args[]) {
+        System.out.println("Hello " + args[0]);
+        System.out.println(new Date());
+    }
+}
